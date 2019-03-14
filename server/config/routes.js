dca2ac87843d1dfb514b36777b86fb89f6ec1e71
prescottbreeden <prescott.breeden@gmail.com>
2019-03-13 17:24:05 -0700

    Updated with Prescott's Comments from Code Review
    He showed me lotses of thingsies

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,19 +1,10 @@
-var tasks = require("../controllers/tasks.js");
+const controllers = require("../controllers/tasks.js");
 
-module.exports = function(app) {
-    app.get('/', function(req, res) {
-        tasks.index(req, res)
-    })
-    app.get('/:id', function(req, res) {
-        tasks.getTask(req, res, req.params.id)
-    })
-    app.delete('/:id', function(req, res) {
-        tasks.deleteTask(req, res, req.params.id)
-    })
-    app.post('/new', function(req, res) {
-        tasks.create(req, res)
-    })
-    app.put('/:id', function(req, res) {
-        tasks.updateTask(req, res, req.params.id)
-    })
+module.exports = app => {
+  app
+    .get('/api/tasks', controllers.getAllTasks)
+    .get('/api/tasks/:id', controllers.getTask)
+    .post('/api/tasks', controllers.create)
+    .put('/api/tasks/:id', controllers.updateTask)
+    .delete('/api/tasks/:id', controllers.deleteTask)
 }
