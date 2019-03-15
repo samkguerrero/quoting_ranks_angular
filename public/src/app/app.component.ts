1ab40ee495a4ad1526e777c87c0a7f0fbe2d6f10
Sam Guerrero <samguerrero@Sams-MacBook-Pro.local>
2019-03-15 14:56:37 -0700

    angular services, managing subscriptions and observable
    in our angular application within our express application, i am managing better how how subscriptions are passed from the service to the angular ts component, in the component i am adding a subscription to the passed over observable from the service, i am then adding the returned data to component to then style in the dom

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -8,7 +8,14 @@ import { HttpService } from './http.service';
 })
 
 export class AppComponent {
+
   title = 'Restful Tasks';
+  all_tasks: object = [];
+  queried_task: object = {};
+
   constructor(private _httpService: HttpService) {
+    _httpService.getTasks().subscribe(data => this.all_tasks = data)
+    _httpService.getTask().subscribe(data => this.queried_task = data)
   }
+
 }
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -8,25 +8,12 @@ export class HttpService {
   constructor(private _http: HttpClient){
     this.getTasks();
     this.getTask();
-    this.deleteTask();
   }
   getTasks(){
-      // our http response is an Observable, store it in a variable
-      let tempObservable = this._http.get('/api/tasks');
-      // subscribe to the Observable and provide the code we would like to do with our data from the response
-      tempObservable.subscribe(data => console.log("Got our tasks!", data));
+      return this._http.get('/api/tasks');
   }
   getTask(){
-      // our http response is an Observable, store it in a variable
-      let tempObservable = this._http.get('/api/tasks/5c89564e0469da6cc6da185c');
-      // subscribe to the Observable and provide the code we would like to do with our data from the response
-      tempObservable.subscribe(data => console.log("Got our task!", data));
-  }
-  deleteTask(){
-      // our http response is an Observable, store it in a variable
-      let tempObservable = this._http.delete('/api/tasks/5c89564e0469da6cc6da185c');
-      // subscribe to the Observable and provide the code we would like to do with our data from the response
-      tempObservable.subscribe(data => console.log("deleted task!", data));
+      return this._http.get('/api/tasks/5c896c7e227980b0441aef25');
   }
 }
 
